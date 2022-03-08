cb98aeca7e365e79817ecfb0bee208f05969503c
mspiderv <mspiderv@gmail.com>
2022-03-08 19:09:48 +0100

    codestyle

--- config/settings-api.php
+++ config/settings-api.php
@@ -27,15 +27,15 @@ return [
     'implementations' => [
 
         // Controller
-        'controller' => BonsaiCms\SettingsApi\Http\Controllers\SettingsController::class,
+        'controller' => \BonsaiCms\SettingsApi\Http\Controllers\SettingsController::class,
 
         // Requests
-        BonsaiCms\SettingsApi\Contracts\ReadSettingsRequestContract::class => BonsaiCms\SettingsApi\Http\Requests\ReadSettingsRequest::class,
-        BonsaiCms\SettingsApi\Contracts\WriteSettingsRequestContract::class => BonsaiCms\SettingsApi\Http\Requests\WriteSettingsRequest::class,
+        BonsaiCms\SettingsApi\Contracts\ReadSettingsRequestContract::class => \BonsaiCms\SettingsApi\Http\Requests\ReadSettingsRequest::class,
+        BonsaiCms\SettingsApi\Contracts\WriteSettingsRequestContract::class => \BonsaiCms\SettingsApi\Http\Requests\WriteSettingsRequest::class,
 
         // Responses
-        BonsaiCms\SettingsApi\Contracts\ReadSettingsResponseContract::class => BonsaiCms\SettingsApi\Http\Responses\ReadSettingsResponse::class,
-        BonsaiCms\SettingsApi\Contracts\WriteSettingsResponseContract::class => BonsaiCms\SettingsApi\Http\Responses\WriteSettingsResponse::class,
+        BonsaiCms\SettingsApi\Contracts\ReadSettingsResponseContract::class => \BonsaiCms\SettingsApi\Http\Responses\ReadSettingsResponse::class,
+        BonsaiCms\SettingsApi\Contracts\WriteSettingsResponseContract::class => \BonsaiCms\SettingsApi\Http\Responses\WriteSettingsResponse::class,
 
     ],
 
